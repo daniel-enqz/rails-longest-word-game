65ce4be283ce82d048901c5bfaf3a4c8c06842f1
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-28 15:15:50 -0600

    files added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,12 @@ class GamesController < ApplicationController
   end
 
   def score
-    raise
+    @guess = params[:guess]
+    @letters = params[:letters]
+    @result = if @guess.chars.all? { |letter| @guess.count(letter) <= @letters.count(letter.upcase) }
+                'Congratulations! ${@guess} is a valid English word!'
+              else
+                'NOT VALID'
+              end
   end
 end
