1a91bb5a1c30199f16ef3b92a8f7191273ec8f08
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-28 17:27:10 -0600

    files added

--- test/system/games_test.rb
+++ test/system/games_test.rb
@@ -1,9 +1,9 @@
 require "application_system_test_case"
 
 class GamesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit games_url
-  #
-  #   assert_selector "h1", text: "Games"
-  # end
+  test "Going to /new gives us a new random grid to play with" do
+    visit new_url
+    assert test: "New game"
+    assert_selector "li", count: 10
+  end
 end
