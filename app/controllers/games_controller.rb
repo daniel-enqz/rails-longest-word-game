e58336aa5932360c3e5d5e4dae5f47390f97501d
Daniel Enriquez <daniel.enm17@gmail.com>
2022-02-28 17:00:10 -0600

    files added

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,16 +20,24 @@ class GamesController < ApplicationController
   end
 
   def analysis(user, guess, letters)
+    session[:past_wins] ||= []
     if guess.chars.all? { |letter| guess.count(letter) <= letters.count(letter.upcase) } && user['found'] == true
+      session[:past_wins] << guess.length
       "Congratulations! #{guess} is a valid English word!"
     elsif user['found'] == false
+      session[:past_wins] << 0
       'not an english word'
     else
+      session[:past_wins] << 0
       "Sorry, but #{@guess} can't be build out of the grid"
     end
   end
 
   def result
     @result = params[:message]
+  end
+
+  def show
+    @final_score = session[:past_wins].sum
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/new', to: 'games#new'
   post '/score', to: 'games#score'
   get '/result', to: 'games#result'
+  get '/show', to: 'games#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 end
